63f052c4227c6e94600ba2a2ae2f143b7b0bb95a
Iain Knox <iain.knox1@btinternet.com>
2022-07-28 22:04:28 +0000

    create function to handle exceptions and return the results to a modal for the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,6 +40,7 @@ async function postForm(e) {
     if (response.ok) {
         displayErrors(data);
     } else {
+        displayException(data); 
         throw new Error(data.error);
     }
 
@@ -55,6 +56,7 @@ async function getStatus(e) {
     if (response.ok) {
         displayStatus(data.expiry);
     } else {
+        displayException(data);
         throw new Error(data.error);
     }
 }
@@ -90,6 +92,21 @@ function displayErrors(data) {
     document.getElementById('results-content').innerHTML = results;
 
     resultsModal.show();
+
+}
+
+function displayException(data) {
+
+    let heading = `An Expection Occurred`;
+    
+    results = `<div>The API returned status code ${data.status_code}</div>`;
+    results += `<div>Error Number: <strong>${data.error_no}</strong></div>`;
+    results += `<div>Error text: <strong>${data.error}</strong></div>`;
+
+    document.getElementById('resultsModalTitle').innerText = heading;
+    document.getElementById('results-content').innerHTML = results;
+
+    resultsModal.show(); 
 
 }
 

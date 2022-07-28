29f5447375b4502c938151cb3a27c63729ed1ad6
Iain Knox <iain.knox1@btinternet.com>
2022-07-28 21:02:25 +0000

    create post form function that posts to the API_URL

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ const API_URL = 'https://ci-jshint.herokuapp.com/api';
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener('click', e => getStatus(e));
+document.getElementById('submit').addEventListener('click', e => postForm(e));
 
 async function getStatus(e) {
     const queryString = `${API_URL}?api_key=${API_KEY}`;
@@ -30,14 +31,18 @@ function displayStatus(data) {
     resultsModal.show();
 }
 
-// function displayStatus(data) {
 
-//     let heading = "API Key Status";
-//     let results = `<div>Your key is valid until</div>`;
-//     results += `<div class="key-status">${data.expiry}</div>`;
 
-//     document.getElementById("resultsModalTitle").innerText = heading;
-//     document.getElementById("results-content").innerHTML = results;
-//     resultsModal.show();
+async function postForm(e) {
+    const form = new FormData(document.getElementById('checksform'));
 
-// }
+    
+    const response = await fetch("API_URL", {
+                        method: "POST",
+                        headers: {
+                                    "Authorization": API_KEY,
+        },
+                                 body: form, 
+    })
+
+}
